e04e07fc6cbf41f84d06ca45ad3ab970ac2ba768
Jana1902 <jana7cr@gmail.com>
2025-06-20 16:38:10 +0530

    Added Cors

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ const { open } = require("sqlite");
 const sqlite3 = require("sqlite3");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 let dbPath = path.join(__dirname, "database.db");
 let db = null;
