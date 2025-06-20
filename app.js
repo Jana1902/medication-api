17f026e41c1a1fc5b409fdb941504956e120da11
Jana1902 <jana7cr@gmail.com>
2025-06-20 17:20:12 +0530

    REversed Cors

--- app.js
+++ app.js
@@ -7,11 +7,7 @@ const jwt = require("jsonwebtoken");
 const cors = require("cors");
 
 const app = express();
-app.use(
-  cors({
-    origin: ["http://localhost:3000"],
-  })
-);
+app.use(cors());
 app.use(express.json());
 
 let dbPath = path.join(__dirname, "database.db");
